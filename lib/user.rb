a9a1d15ee58cbe8559edfb2634de3838d3b042ac
Brian Fan and Dan Jacob <dan@>
2017-05-16 13:16:01 -0700

    change validate method for password and username length

--- lib/user.rb
+++ lib/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :languages
   validates :name, :presence => true
   validates :login, :uniqueness => true,
-                    :length => {:minimum => 3, :maximum => 10}
+                    :length => {:minimum => 5, :maximum => 10}
   validates :password, :presence => true,
-                       :length => {:minimum => 3, :maximum => 10}
+                       :length => {:minimum => 5, :maximum => 10}
 end
