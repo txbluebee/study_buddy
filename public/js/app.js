c5b9c6557dd66329054168f4a135b67fc38d7a1d
Anatoliy Klots and Anna Horodetska <anatoliy+anna@>
2017-05-16 14:12:31 -0700

    try to add flip to flashcards

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
- $(document).ready(function() {
-  // $('.flashcard').on('click', function() {
-  //   $('.flashcard').toggleClass('flipped');
-  // });
-  $("#card").flip();
+$(document).ready(function() {
+  $(this).on('click', function() {
+
+    var i = $(this).attr('id');
+  
+    $(this).toggle('.flipped');
+  });
 });
+
+
+  // $(this).next(".card").flip();
+
+
+  // $(document).ready(function() {
+  //   $('.flashcard').on('click', function() {
+  //     $('.flashcard').toggleClass('flipped');
+  //   });
+  // });
