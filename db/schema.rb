3ec97d803636dab9c0054bbc14e0d97f74f74e69
Guest User <Guest@epicodus-2.local>
2017-05-18 11:58:02 -0700

    add routing to resources, flashcard background

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515205226) do
+ActiveRecord::Schema.define(version: 20170518183855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 20170515205226) do
     t.text "description"
     t.string "github"
     t.integer "language_id"
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
+    t.string "link"
+    t.string "language_id"
   end
 
   create_table "tags", force: :cascade do |t|
